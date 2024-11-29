57043add8356df81091c71293048218d11663c2f
Himaanshusingh12 <himaanshu1601@gmail.com>
2024-11-29 14:58:51 +0530

    profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -7,10 +7,12 @@ import { useNavigate } from "react-router-dom";
 function Profile() {
   const [profileData, setProfileData] = useState(null);
   const [showChangePassword, setShowChangePassword] = useState(false);
+  const [showEditProfile, setShowEditProfile] = useState(false); //this
   const [oldPassword, setOldPassword] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [editMessage, setEditMessage] = useState("");
 
   const [activeButton, setActiveButton] = useState("profile");
   // State to manage password visibility
@@ -45,18 +47,60 @@ function Profile() {
   // Handle the toggle for displaying the Change Password form
   const handleChangePasswordClick = () => {
     setShowChangePassword(true);
+    setShowEditProfile(false);
     setActiveButton("changePassword");
   };
 
   const handleProfileClick = () => {
     setShowChangePassword(false);
+    setShowEditProfile(false);
     setActiveButton("profile");
+  };
+
+  //Function to handle Edit profile button click
+  const handleEditProfileClick = () => {
+    setShowEditProfile(true);
+    setShowChangePassword(false);
+    setActiveButton("editProfile");
   };
 
   //Logout function with navigate
   const handleLogout = () => {
     localStorage.removeItem("user");
-    navigate("/login");
+    navigate("/");
+  };
+
+  // edit profile
+  const handleEditProfileSubmit = async (e) => {
+    e.preventDefault();
+
+    const userData = JSON.parse(localStorage.getItem("user"));
+    const userId = userData ? userData.userid : null;
+
+    if (!userId) {
+      setEditMessage("User  not found. Please login again.");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "https://nodesolution.in/etworld/update_profile.php", // Replace with your actual API endpoint
+        new URLSearchParams({
+          update_profile: true,
+          userid: userId,
+          first_name: profileData.first_name,
+          last_name: profileData.last_name,
+        })
+      );
+
+      if (response.data.response === "1") {
+        setEditMessage("Profile updated successfully!");
+      } else {
+        setEditMessage("Failed to update profile: " + response.data.message);
+      }
+    } catch (error) {
+      setEditMessage("Error updating profile. Please try again later.");
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -89,7 +133,7 @@ function Profile() {
 
       if (response.data.response === "1") {
         setMessage("Password updated successfully!");
-        navigate("/");
+        navigate("/home");
       } else {
         setMessage("Failed to update password: " + response.data.message);
       }
@@ -130,6 +174,14 @@ function Profile() {
                 </button>
                 <button
                   className={`list-group-item list-group-item-action ${
+                    activeButton === "editProfile" ? "active bg-primary" : ""
+                  }`}
+                  onClick={handleEditProfileClick}
+                >
+                  Edit Profile
+                </button>
+                <button
+                  className={`list-group-item list-group-item-action ${
                     activeButton === "changePassword" ? "active bg-primary" : ""
                   }`}
                   onClick={handleChangePasswordClick}
@@ -155,11 +207,18 @@ function Profile() {
                 <h4 className="card-title text-center mb-4">
                   {showChangePassword ? "Change Your Password" : "User Profile"}
                 </h4>
-
                 {/* Conditionally render Profile or Change Password form */}
                 {showChangePassword ? (
                   <div>
-                    <form className="was-validated" onSubmit={handleSubmit}>
+                    <form
+                      className="was-validated"
+                      onSubmit={handleSubmit}
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        padding: "0 50px",
+                      }}
+                    >
                       <div
                         className="mb-3 mt-3"
                         style={{ position: "relative" }}
@@ -169,9 +228,7 @@ function Profile() {
                             border: "none",
                             borderBottom: "1px solid black",
                             textDecoration: "none",
-                            width: "50%",
-                            // marginLeft: "150px",
-                            marginLeft: "100px",
+                            width: "100%",
                           }}
                           type={showOldPassword ? "text" : "password"}
                           className="form-control"
@@ -188,7 +245,7 @@ function Profile() {
                           className="btn btn-link"
                           style={{
                             position: "absolute",
-                            right: "280px",
+                            right: "20px",
                             top: "2px",
                             fontSize: "16px",
                             background: "none",
@@ -197,9 +254,9 @@ function Profile() {
                           }}
                         >
                           {showOldPassword ? (
-                            <i className="fas fa-eye-slash"></i>
-                          ) : (
                             <i className="fas fa-eye"></i>
+                          ) : (
+                            <i className="fas fa-eye-slash"></i>
                           )}
                         </button>
                       </div>
@@ -210,8 +267,7 @@ function Profile() {
                             border: "none",
                             borderBottom: "1px solid black",
                             textDecoration: "none",
-                            width: "50%",
-                            marginLeft: "100px",
+                            width: "100%",
                           }}
                           type={showNewPassword ? "text" : "password"}
                           className="form-control"
@@ -228,7 +284,7 @@ function Profile() {
                           className="btn btn-link"
                           style={{
                             position: "absolute",
-                            right: "280px",
+                            right: "20px",
                             top: "2px",
                             fontSize: "16px",
                             background: "none",
@@ -237,9 +293,9 @@ function Profile() {
                           }}
                         >
                           {showNewPassword ? (
-                            <i className="fas fa-eye-slash"></i>
-                          ) : (
                             <i className="fas fa-eye"></i>
+                          ) : (
+                            <i className="fas fa-eye-slash"></i>
                           )}
                         </button>
                       </div>
@@ -250,8 +306,7 @@ function Profile() {
                             border: "none",
                             borderBottom: "1px solid black",
                             textDecoration: "none",
-                            width: "55%",
-                            marginLeft: "100px",
+                            width: "100%",
                           }}
                           type={showConfirmPassword ? "text" : "password"}
                           className="form-control"
@@ -268,7 +323,7 @@ function Profile() {
                           className="btn btn-link"
                           style={{
                             position: "absolute",
-                            right: "280px",
+                            right: "20px",
                             top: "2px",
                             fontSize: "16px",
                             background: "none",
@@ -277,17 +332,17 @@ function Profile() {
                           }}
                         >
                           {showConfirmPassword ? (
-                            <i className="fas fa-eye-slash"></i>
-                          ) : (
                             <i className="fas fa-eye"></i>
+                          ) : (
+                            <i className="fas fa-eye-slash"></i>
                           )}
                         </button>
                       </div>
 
                       <button
                         type="submit"
-                        className="btn btn-primary w-50"
-                        style={{ borderRadius: "15px", marginLeft: "100px" }}
+                        className="btn btn-primary"
+                        style={{ borderRadius: "15px" }}
                       >
                         Change Password
                       </button>
@@ -297,6 +352,110 @@ function Profile() {
                     )}
 
                     <hr className="my-4" />
+                  </div>
+                ) : showEditProfile ? (
+                  <div>
+                    <form
+                      onSubmit={handleEditProfileSubmit}
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        padding: "0 50px",
+                      }}
+                    >
+                      <div className="mb-3">
+                        <h5>
+                          Unique ID:{" "}
+                          {profileData ? profileData.unique_id : "N/A"}
+                        </h5>
+                      </div>
+
+                      <div className="mb-3">
+                        <input
+                          style={{
+                            border: "none",
+                            borderBottom: "1px solid black",
+                            textDecoration: "none",
+                            width: "100%",
+                          }}
+                          type="text"
+                          className="form-control"
+                          placeholder="First Name"
+                          value={profileData ? profileData.first_name : ""}
+                          onChange={(e) =>
+                            setProfileData({
+                              ...profileData,
+                              first_name: e.target.value,
+                            })
+                          }
+                          required
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <input
+                          style={{
+                            border: "none",
+                            borderBottom: "1px solid black",
+                            textDecoration: "none",
+                            width: "100%",
+                          }}
+                          type="text"
+                          className="form-control"
+                          placeholder="Last Name"
+                          value={profileData ? profileData.last_name : ""}
+                          onChange={(e) =>
+                            setProfileData({
+                              ...profileData,
+                              last_name: e.target.value,
+                            })
+                          }
+                          required
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <input
+                          style={{
+                            border: "none",
+                            borderBottom: "1px solid black",
+                            textDecoration: "none",
+                            width: "100%",
+                          }}
+                          type="text"
+                          className="form-control"
+                          placeholder="Email"
+                          value={profileData ? profileData.email : ""}
+                          readOnly
+                        />
+                      </div>
+
+                      {/* Phone - Read Only */}
+                      <div className="mb-3">
+                        <input
+                          style={{
+                            border: "none",
+                            borderBottom: "1px solid black",
+                            textDecoration: "none",
+                            width: "100%",
+                          }}
+                          type="text"
+                          className="form-control"
+                          placeholder="Phone"
+                          value={profileData ? profileData.mobile : ""}
+                          readOnly
+                        />
+                      </div>
+
+                      <button
+                        type="submit"
+                        className="btn btn-primary w-40"
+                        style={{ borderRadius: "15px" }}
+                      >
+                        Save Changes
+                      </button>
+                    </form>
+                    {editMessage && (
+                      <div className="mt-3 alert alert-info">{editMessage}</div>
+                    )}
                   </div>
                 ) : (
                   <div className="list-group">
